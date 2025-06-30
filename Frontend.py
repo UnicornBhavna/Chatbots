99eedc647f9bc0f73fea43de571958370b88c854
UnicornBhavna <bhavu1998@gmail.com>
2025-06-30 23:58:22 +0800

    Update Frontend.py

--- Frontend.py
+++ Frontend.py
@@ -2,11 +2,13 @@ import streamlit as st
 from sentence_transformers import SentenceTransformer
 import faiss, pickle, openai, os, numpy as np, time
 import requests
+import os
 
 
 # === Load API Key ===
 try:
-    openai.api_key = st.secrets["OPENAI_API_KEY"]
+    #openai.api_key = st.secrets["OPENAI_API_KEY"]
+    openai.api_key = os.getenv("OPENAI_API_KEY")
 except KeyError:
     st.error("OpenAI API key not found. Please add it in Streamlit Secrets.")
     st.stop()
